85bf28f4e611e4d3ce1dd98ff74e682c6cd994be
Clement Calmels <clement.calmels@free.fr>
2026-09-17 10:57:06 +0200

    test: add automatic test for chip8-logo rom

---
+++ tests/chip8_test_suite.rs
@@ -0,0 +1,21 @@
+use chip_8::machine::Chip8;
+use chip_8::windowing::Updatable;
+
+fn assert_rom_matches_expected(rom: &[u8], expected: &[u8], steps: usize) {
+    let mut chip8 = Chip8::from_rom(rom).unwrap();
+
+    for _ in 0..steps {
+        chip8.update();
+    }
+
+    assert_eq!(chip8.framebuffer(), expected);
+}
+
+#[test]
+fn chip8_logo() {
+    assert_rom_matches_expected(
+        include_bytes!("fixtures/1-chip8-logo.ch8"),
+        include_bytes!("fixtures/1-chip8-logo.expected"),
+        39, // this number comes from Timendus CHIP-8 splash screen description
+    );
+}
