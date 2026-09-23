052e764ea73f096e1c16352a286be1529a72d329
Clement Calmels <clement.calmels@free.fr>
2026-09-23 15:11:20 +0200

    feat: add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use chip_8::machine::Chip8;
 use chip_8::windowing::App;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg()]
+    rom_path: String,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rom = std::fs::read("./tests/fixtures/pong.ch8")?;
+    let args = Args::parse();
+    let rom = std::fs::read(args.rom_path)?;
     let mut app = App::new(Chip8::from_rom(&rom)?);
 
     app.run();
